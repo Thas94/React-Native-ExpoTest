ea458cf907f82b41f6cbe92166521786001ac841
NTC\0002228650 <Thabiso.Malematja@netcare.co.za>
2024-03-08 14:49:04 +0200

    Stack routing

--- src/app/days/day1/index.tsx
+++ src/app/days/day1/index.tsx
@@ -1,9 +1,11 @@
-import { View , Text} from "react-native";
+import { View ,Text} from "react-native";
+import { Stack } from "expo-router";
 
 
 export default function Day1(){
     return (
         <View>
+            <Stack.Screen options={{title: 'Day 1'}}/>
             <Text>day 11111111</Text>
         </View>
     );
